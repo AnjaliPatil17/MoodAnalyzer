77f5ce6addfff0e6a5dbf259517206c525bc7b41
Anjali <anjaliap1227@gmail.com>
2024-05-28 10:24:34 +0530

    MoodAnalyzer

---
+++ src/main/java/MoodAnalyzer.java
@@ -0,0 +1,20 @@
+public class MoodAnalyzer {
+
+    private String msg;
+
+    public MoodAnalyzer(){
+
+    }
+    public MoodAnalyzer(String msg){
+        this.msg = msg;
+    }
+
+    public String analyzeMood(){
+        if(msg.contains("sad")){
+            return "SAD";
+        }
+        else {
+            return "HAPPY";
+        }
+    }
+}
---
+++ src/test/java/MoodAnalyzerTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MoodAnalyzerTest {
+
+    @Test
+    void analyzeMood() {
+        MoodAnalyzer moodAnalyzer1 = new MoodAnalyzer("I am in Sad mood".toLowerCase());
+        String mood = moodAnalyzer1.analyzeMood();
+        Assertions.assertEquals(mood,"SAD");
+    }
+
+    @Test
+    void analyzeHappyMood() {
+        MoodAnalyzer moodAnalyzer2 = new MoodAnalyzer("I am in Any mood".toLowerCase());
+        String mood = moodAnalyzer2.analyzeMood();
+        Assertions.assertEquals(mood,"HAPPY");
+    }
+
+
+}
